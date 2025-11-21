1d21cb9875761ae284a2ae7d56e26355f6bd3399
Henrique Akira Murakami <henriquemurakami14@hotmail.com>
2025-11-21 11:59:01 -0300

    bjbhj

---
+++ moda-da-mulher/footer.php
@@ -0,0 +1,70 @@
+    <!-- Footer is starting -->
+    <footer class="bg-cor-secundaria text-white">
+
+        <div class="container py-5">
+            <!-- Collumn 1 is starting -->
+            <div class="row">
+
+                <div class="col-md-3">
+
+                    <h5 class="text-primary">Atendimento</h5>
+
+                    <ul>
+                        <li>(11) 99999-9999</li>
+                        <li>email@gmail.com</li>
+                        <li>Segunda a Sexta das 09h00 as 17h00</li>
+                    </ul>
+
+                </div>
+
+                <div class="col-md-3">
+                    
+                    <h5 class="text-primary">Atendimento</h5>
+
+                    <ul>
+                        <li>(11) 99999-9999</li>
+                        <li>email@gmail.com</li>
+                        <li>Segunda a Sexta das 09h00 as 17h00</li>
+                    </ul>
+
+                </div>
+
+                <div class="col-md-3">
+
+                    <h5 class="text-primary">Atendimento</h5>
+
+                    <ul>
+                        <li>(11) 99999-9999</li>
+                        <li>email@gmail.com</li>
+                        <li>Segunda a Sexta das 09h00 as 17h00</li>
+                    </ul>
+
+                </div>
+
+                <div class="col-md-3">
+
+                    <h5 class="text-primary">Atendimento</h5>
+                    
+                    <ul>
+                        <li>(11) 99999-9999</li>
+                        <li>email@gmail.com</li>
+                        <li>Segunda a Sexta das 09h00 as 17h00</li>
+                    </ul>
+
+                </div>
+
+            </div>
+            <!-- Collumn 1 is finished --> 
+            
+        </div>
+
+    </footer>
+     <!-- Footer is finished -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+
+    </script>
+</body>
+
+</html>
---
+++ moda-da-mulher/header.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Moda da Mulher</title>
+    <link href="assets/styles/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/images/favicon/favicon-16x16.png" type="image/png">
+    <link rel="stylesheet" href="assets/styles/app.css">
+    <link rel="stylesheet" href="assets/styles/produto.css">
+</head>
+
+<body>
+
+    <!-- Header is starting -->
+    <header class="bg-cor-primaria">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-lg-2 my-4 text-center text-lg-start">
+                    <img src="assets/images/logo.svg" alt="LogoModaMulher">
+                </div>
+
+                <div class="col-lg-7 my-4 d-flex align-itens-center">
+
+                    <form action="" class="site-search w-100">
+
+                        <div class="input-group text-primary">
+
+                            <input type="text" class="form-control rounded-pill py-2 px-4" placeholder="Choose your style here">
+                            <button class="btn position-absolute"><i class="bi bi-search"></i></button>
+
+                        </div>
+
+                    </form>
+
+                </div>
+
+                <div class="col-lg-3 d-flex align-items-center justify-content-end">
+
+                    <ul class="nav">
+
+                        <li class="nav-item">
+
+                            <a class="nav-link text-secondary" href="#"><i class="bi bi-person-fill"></i> Entrar</a>
+
+                        </li>   
+
+                        <li class="nav-item">
+                            <a class="nav-link text-secondary" href="#"><i class="bi bi-cart-fill position-relative">
+                                    <span class="badge position-absolute top-0 start-100 translate-middle bg-danger rounded-circle">3</span>
+                                </i> Carrinho</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </header>
+    <!-- Header is finished -->
+
+    <!-- NavBar is starting -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-cor-secundaria sticky-top">
+
+        <div class="container">
+
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu-principal">
+                <i class="navbar-toggler-icon"></i>
+            </button>
+
+            <div id="menu-principal" class="collapse navbar-collapse">
+
+                <ul class="navbar-nav">
+
+                    <li class="nav-item dropdown">
+
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Moda Feminina</a>
+
+                        <ul class="dropdown-menu">
+
+                            <li><a class="dropdown-item" href="#"></a>Lançamentos</li>
+                            <li><a class="dropdown-item" href="#"></a>Moda Casual</li>
+                            <li><a class="dropdown-item" href="#"></a>Moda Praia</li>
+
+                        </ul>
+
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Calçados</a>
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </nav>
+    <!-- NavBar is finished -->
